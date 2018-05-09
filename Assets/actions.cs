737720f41f1916a29f40dbee27d8bb7bf4695f41
Javier Roque <jarb95@gmail.com>
2018-05-09 01:08:59 -0600

    Added straight punch animations for player fists
    Triggers were also included.

---
+++ Assets/actions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actions : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow))
+        {
+            
+            //Send the message to the Animator to activate the trigger parameter named "Jump"
+            anim.SetTrigger("lfist_straight_punch");
+        }
+	}
+
+	// void lfist_straight_punch(){
+	// 	 anim.SetTrigger("lfist_straight_punch");
+	// }
+}
