5b1de41a4f6e4907888d63765eeb2598ceb3cfa7
javier-montoya <javier.montoya@partnerhero.com>
2018-05-09 21:15:05 -0600

    Added sprite for side punch

--- Assets/Scripts/Player/ActionListener.cs
+++ Assets/Scripts/Player/ActionListener.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class actions : MonoBehaviour {
+public class ActionListener : MonoBehaviour {
 
 	Animator anim;
 	// Use this for initialization
