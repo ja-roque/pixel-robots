e2dea7762a8334cb25b3d99d230c75304a94a33c
javier-montoya <javier.montoya@partnerhero.com>
2018-05-21 16:43:37 -0600

    added stuff for collisions

---
+++ Assets/CollisionListener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionListener : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision col){
+		Debug.Log ("pega porque es gorda");
+		anim = col.gameObject.GetComponentInParent(typeof(Animator)) as Animator;
+		string clipInfo = anim.GetCurrentAnimatorClipInfo(0)[0].clip + "";
+//		if(clipInfo.Contains(left))
+	}
+}
