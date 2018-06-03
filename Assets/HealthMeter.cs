e1132f3b1aee165c4df62ac4a153b07aa329ce74
Javier Roque <jarb95@gmail.com>
2018-06-03 16:40:50 -0600

    Positioned health bar sprites and added component to health object for auto-scaling.

---
+++ Assets/HealthMeter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMeter : MonoBehaviour {
+
+	Transform 	healthMask;
+	float 		healthScaleSize = 2f;
+
+	// Use this for initialization
+	void Start () {
+		healthMask = this.gameObject.transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateHealthBar();
+	}
+
+	void updateHealthBar(){
+		healthMask.localScale = new Vector3(healthScaleSize * (PlayerDataManager.playerHealth.value/100f), 1, 1);
+	}
+}
